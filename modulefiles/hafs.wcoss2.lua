597bbc7e3012b5d90118907dcd2fcddd211f687e
Keqin Wu <Keqin.Wu@gaea65.ncrc.gov>
2025-08-25 02:41:04 -0400

    Add jobs modulefiles parm rocoto

---
+++ modulefiles/hafs.wcoss2.lua
@@ -0,0 +1,142 @@
+help([[
+loads HAFS prerequisites on Cactus and Dogwood
+]])
+
+PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver")
+load(pathJoin("PrgEnv-intel", PrgEnv_intel_ver))
+
+intel_ver=os.getenv("intel_ver")
+load(pathJoin("intel", intel_ver))
+
+craype_ver=os.getenv("craype_ver")
+load(pathJoin("craype", craype_ver))
+
+cray_mpich_ver=os.getenv("cray_mpich_ver")
+load(pathJoin("cray-mpich", cray_mpich_ver))
+
+cray_pals_ver=os.getenv("cray_pals_ver")
+load(pathJoin("cray-pals", cray_pals_ver))
+
+cmake_ver=os.getenv("cmake_ver")
+load(pathJoin("cmake", cmake_ver))
+
+python_ver=os.getenv("python_ver")
+load(pathJoin("python", python_ver))
+
+jasper_ver=os.getenv("jasper_ver")
+load(pathJoin("jasper", jasper_ver))
+
+zlib_ver=os.getenv("zlib_ver")
+load(pathJoin("zlib", zlib_ver))
+
+libpng_ver=os.getenv("libpng_ver")
+load(pathJoin("libpng", libpng_ver))
+
+libjpeg_ver=os.getenv("libjpeg_ver")
+load(pathJoin("libjpeg", libjpeg_ver))
+
+hdf5_ver=os.getenv("hdf5_ver")
+load(pathJoin("hdf5-C", hdf5_ver))
+
+netcdf_ver=os.getenv("netcdf_ver")
+load(pathJoin("netcdf-C", netcdf_ver))
+
+fms_ver=os.getenv("fms_ver")
+load(pathJoin("fms-C", fms_ver))
+
+bacio_ver=os.getenv("bacio_ver")
+load(pathJoin("bacio", bacio_ver))
+
+crtm_ver=os.getenv("crtm_ver")
+load(pathJoin("crtm", crtm_ver))
+
+g2_ver=os.getenv("g2_ver")
+load(pathJoin("g2", g2_ver))
+
+g2tmpl_ver=os.getenv("g2tmpl_ver")
+load(pathJoin("g2tmpl", g2tmpl_ver))
+
+bufr_ver=os.getenv("bufr_ver")
+load(pathJoin("bufr", bufr_ver))
+
+ip_ver=os.getenv("ip_ver")
+load(pathJoin("ip", ip_ver))
+
+sp_ver=os.getenv("sp_ver")
+load(pathJoin("sp", sp_ver))
+
+w3emc_ver=os.getenv("w3emc_ver")
+load(pathJoin("w3emc", w3emc_ver))
+
+w3nco_ver=os.getenv("w3nco_ver")
+load(pathJoin("w3nco", w3nco_ver))
+
+nemsio_ver=os.getenv("nemsio_ver")
+load(pathJoin("nemsio", nemsio_ver))
+
+sigio_ver=os.getenv("sigio_ver")
+load(pathJoin("sigio", sigio_ver))
+
+sfcio_ver=os.getenv("sfcio_ver")
+load(pathJoin("sfcio", sfcio_ver))
+
+wrf_io_ver=os.getenv("wrf_io_ver")
+load(pathJoin("wrf_io", wrf_io_ver))
+
+prod_util_ver=os.getenv("prod_util_ver")
+load(pathJoin("prod_util", prod_util_ver))
+
+grib_util_ver=os.getenv("grib_util_ver")
+load(pathJoin("grib_util", grib_util_ver))
+
+wgrib2_ver=os.getenv("wgrib2_ver")
+load(pathJoin("wgrib2", wgrib2_ver))
+
+cfp_ver=os.getenv("cfp_ver")
+load(pathJoin("cfp", cfp_ver))
+
+udunits_ver=os.getenv("udunits_ver")
+load(pathJoin("udunits", udunits_ver))
+
+gsl_ver=os.getenv("gsl_ver")
+load(pathJoin("gsl", gsl_ver))
+
+nco_ver=os.getenv("nco_ver")
+load(pathJoin("nco", nco_ver))
+
+bufr_dump_ver=os.getenv("bufr_dump_ver")
+load(pathJoin("bufr_dump", bufr_dump_ver))
+
+cdo_ver=os.getenv("cdo_ver")
+load(pathJoin("cdo", cdo_ver))
+
+ncdiag_ver=os.getenv("ncdiag_ver")
+load(pathJoin("ncdiag-A", ncdiag_ver))
+
+ncio_ver=os.getenv("ncio_ver")
+load(pathJoin("ncio-A", ncio_ver))
+
+pio_ver=os.getenv("pio_ver")
+load(pathJoin("pio-C", pio_ver))
+
+esmf_ver=os.getenv("esmf_ver")
+load(pathJoin("esmf-C", esmf_ver))
+
+gftl_shared_ver=os.getenv("gftl_shared_ver")
+--load(pathJoin("gftl-shared", gftl_shared_ver))
+
+mapl_ver=os.getenv("mapl_ver")
+--load(pathJoin("mapl-C", mapl_ver))
+
+scotch_ver=os.getenv("scotch_ver")
+load(pathJoin("scotch", scotch_ver))
+
+setenv("CC", "cc")
+setenv("CXX", "CC")
+setenv("FC", "ftn")
+setenv("CMAKE_C_COMPILER", "cc")
+setenv("CMAKE_CXX_COMPILER", "CC")
+setenv("CMAKE_Fortran_COMPILER", "ftn")
+setenv("CMAKE_Platform", "wcoss2")
+
+whatis("Description: HAFS build/run environment")
---
+++ rocoto/cronjob_arafs_alaska_3.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+set -x
+date
+
+#HOMEhafs=${HOMEhafs:-/gpfs/f6/drsa-hurr2/world-shared/save/Jiayi.Peng/hafsU4km}
+#export HOMEhafs=/gpfs/f6/drsa-hurr2/world-shared/save/Jiayi.Peng/hafsU3km
+#source ${HOMEhafs}/ush/hafs_pre_job.sh.inc
+
+export HOMEhafs=${HOMEhafs:-/gpfs/f6/drsa-hurr4/scratch/${USER}/arafs3km}
+source ${HOMEhafs}/ush/hafs_pre_job.sh.inc
+
+cd ${HOMEhafs}/rocoto
+EXPT=$(basename ${HOMEhafs})
+SUBEXPT=ARAFS_Exp4_alaska_3
+
+scrubopt="config.scrub_work=no config.scrub_com=no config.archive=none"
+opts="-t -f -s sites/gaeaC6_ensemble.ent"
+#===============================================================================
+
+ # hafsv0p2a phase2
+ confopts="config.EXPT=${EXPT} config.SUBEXPT=${H225_ensemble} \
+	 ../parm/arafs_exp4_alaska_3.conf "
+#     ../parm/hafs_2024_ensemble_mom6ZI_AL.conf "
+#for ens in 00 01 02 03 04 05 11 12 13 14 15 16 17 18 19 20
+#for ens in 02	
+
+
+./run_arafs.py ${opts} 2023010600 00E HISTORY \
+     ${confopts} ${scrubopt} config.cpu_account=drsa-hurr4 \
+     config.EXPT=${EXPT} config.SUBEXPT=${SUBEXPT}_a config.ENS=99 \
+     config.GFSVER=PROD2021 \
+     config.run_analysis_merge=no \
+     forecast.do_sppt=.false. forecast.do_shum=.false. forecast.do_skeb=.false. \
+     forecast.lndp_type=0 \
+     forecast.write_groups=3 forecast.write_tasks_per_group=40 rocotostr.FORECAST_RESOURCES=FORECAST_RESOURCES_1200PE \
+     forecast.all_tasks=1200 forecast.atm_tasks=1200 forecast.ocn_tasks=0 \
+     forecast.restart_interval=240 config.NHRS=120 config.NOUTHRS=3 config.NBDYHRS=3
+     
+
+
+arafs_exp4_alaska_3.conf
+
+#===============================================================================
+
+date
+
+echo 'cronjob done'
